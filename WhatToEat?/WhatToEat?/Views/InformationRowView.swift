a967766fec84bc3e11bb340047a8e73fa2a1e727
Rookie <jisu7408@gmail.com>
2022-05-08 11:03:54 +0900

    5.08 GitHub check
    **
    깃허브 올린 뒤, 실수로 infoView 코드가 날아감. 그래서 복붙하고 다시 올림

--- WhatToEat?/WhatToEat?/Views/InformationRowView.swift
+++ WhatToEat?/WhatToEat?/Views/InformationRowView.swift
@@ -1,1 +1,26 @@
 
+import SwiftUI
+
+struct InformationRowView: View {
+    
+    var restaurant : RestaurantData
+    
+    
+    var body: some View {
+        HStack {
+            restaurant.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(restaurant.name)
+                .customInfoContent2()
+            Spacer()
+               }
+    }
+}
+
+struct InformationRowView_Previews: PreviewProvider {
+    static var restaurants = ModelData().restaurants
+    static var previews: some View {
+        InformationRowView(restaurant: restaurants[0])
+    }
+}
